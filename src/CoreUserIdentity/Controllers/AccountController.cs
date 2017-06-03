d45d077e5f6cf097d18cb1ac1f010288ecc0f528
Sean <seon2you@hotmail.com>
2017-06-03 13:45:23 -0700

    modified login

--- src/CoreUserIdentity/Controllers/AccountController.cs
+++ src/CoreUserIdentity/Controllers/AccountController.cs
@@ -34,6 +34,21 @@ namespace CoreUserIdentity.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel details, string returnUrl)
         {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await _userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(user, details.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+
+                ModelState.AddModelError(nameof(LoginModel.Email), "Invalid user or password");
+            }
             return View(details);
         }
     }
